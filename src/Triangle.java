bff1319359f31fe9a428e1c48444cdf94a04795f
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-13 08:55:49 +0200

    Homework 4.3 Class Triangle optimisation (fielf height was removed)

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,17 +2,17 @@ public class Triangle {
     double siteA;
     double siteB;
     double siteC;
-    double height;
+//    double height;
 
-    Triangle(double siteA, double siteB, double siteC, double height) {
+    Triangle(double siteA, double siteB, double siteC /*, double height*/) {
         this.siteA = siteA;
         this.siteB = siteB;
         this.siteC = siteC;
-        this.height = height;
+//        this.height = height;
     }
 
     @Override
     public String toString() {
-        return "Trójkąt o bokach a= " + siteA + ", b = " + siteB + ", c = " + siteC + " i wysokości = " + height;
+        return "Trójkąt o bokach a= " + siteA + ", b = " + siteB + ", c = " + siteC /*+ " i wysokości = " + height*/;
     }
 }
--- src/TriangleTest.java
+++ src/TriangleTest.java
@@ -1,6 +1,6 @@
 public class TriangleTest {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(8, 6,10, 8);
+        Triangle triangle = new Triangle(8, 6,10/*, 8*/);
         TriangleStudy triangleStudy = new TriangleStudy();
         System.out.println(triangle+ " jest prostokątny ? " + triangleStudy.isRightTriangle(triangle));
     }
